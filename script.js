9b55d43b6e34efabd31b4c061f93d4e7d04c89d8
a1d3n <$>
2022-06-07 06:14:41 -0500

    complete transformation

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ function copy(that) {
 }
 
 function toast() {
-    var x = document.getElementById("snackbar");
+    var x = document.getElementById("toast");
 
     x.className = "show";
 
     setTimeout(function () {
         x.className = x.className.replace("show", "");
-    },400);
+    },500);
 }
+
+
+// THEME //
+
+
+
